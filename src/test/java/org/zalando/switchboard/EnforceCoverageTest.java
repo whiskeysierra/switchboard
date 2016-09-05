efffbe9b23c814da247d3563b25bae27e111696d
Willi Schönborn <w.schoenborn@gmail.com>
2016-09-05 11:03:26 +0200

    Bumped dependency versions and enabled deprecation errors

--- src/test/java/org/zalando/switchboard/EnforceCoverageTest.java
+++ src/test/java/org/zalando/switchboard/EnforceCoverageTest.java
@@ -10,8 +10,8 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
